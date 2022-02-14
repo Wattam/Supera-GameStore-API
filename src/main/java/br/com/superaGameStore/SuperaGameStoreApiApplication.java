225d51b3c752a8af927751b4b493c52bec776fe6
Nalbert Wattam <nalbertwattam@gmail.com>
2022-02-14 17:10:32 -0300

    feat: add Spring base project

---
+++ src/main/java/br/com/superaGameStore/SuperaGameStoreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.superaGameStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperaGameStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperaGameStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/superaGameStore/SuperaGameStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.superaGameStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperaGameStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
